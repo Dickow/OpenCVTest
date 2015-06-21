dea9df111d30adcc4c79d945ee6af4e14e075268
Dickow <Jeppedic@gmail.com>
2015-06-21 13:30:50 +0200

    tweaked lightning

--- openCVTest/imageProcess/ImageProcessing.java
+++ openCVTest/imageProcess/ImageProcessing.java
@@ -41,7 +41,7 @@ public class ImageProcessing {
 	public int iHighHFront = 108;
 
 	public int iLowS = 96;
-	public int iLowSFront = 47;
+	public int iLowSFront = 67;
 
 	public int iHighS = 255;
 	public int iHighSFront = 255;
@@ -451,35 +451,6 @@ public class ImageProcessing {
 				new Point(goalA.getX() + robot.robotRadius * 3, goalA.getY()
 						+ (robot.robotRadius) * 3));
 
-		// Draw cross
-		// Core.line(image, new Point(cross.getLeftCross().getX(), cross
-		// .getLeftCross().getY()), new Point(
-		// cross.getRightCross().getX(), cross.getRightCross().getY()),
-		// new Scalar(0, 0, 255), 6);
-		// Core.line(image, new Point(cross.getTopCross().getX(), cross
-		// .getTopCross().getY()), new Point(
-		// cross.getBottomCross().getX(), cross.getBottomCross().getY()),
-		// new Scalar(0, 0, 255), 6);
-
-		// draw a rectangle around the cross
-		// Core.rectangle(image, new Point(cross.getLeftCross().getX(), cross
-		// .getTopCross().getY()), new Point(cross.getRightCross().getX(),
-		// cross.getBottomCross().getY()), new Scalar(0, 0, 255));
-		//
-		// // draw lines
-		// Core.line(image, new Point(frames.topLeft().getX(), frames.topLeft()
-		// .getY()), new Point(frames.topRight().getX(), frames.topRight()
-		// .getY()), new Scalar(0, 0, 255), 3);
-		// Core.line(image, new Point(frames.lowLeft().getX(), frames.lowLeft()
-		// .getY()), new Point(frames.lowRight().getX(), frames.lowRight()
-		// .getY()), new Scalar(0, 0, 255), 3);
-		// Core.line(image, new Point(frames.topLeft().getX(), frames.topLeft()
-		// .getY()), new Point(frames.lowLeft().getX(), frames.lowLeft()
-		// .getY()), new Scalar(0, 0, 255), 3);
-		// Core.line(image, new Point(frames.topRight().getX(),
-		// frames.topRight()
-		// .getY()), new Point(frames.lowRight().getX(), frames.lowRight()
-		// .getY()), new Scalar(0, 0, 255), 3);
 		imgCaptures++;
 	}
 
